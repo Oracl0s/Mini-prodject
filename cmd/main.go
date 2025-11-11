ea81b38a91e19904b3bd90f5c50a3f6f50f57df5
Oracl0s <rustem.nuriev.00@inbox.ru>
2025-11-11 10:13:04 +0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,8 @@
 package main
 
-import (
-	"github.com/Oracl0s/Mini-prodject/handlers"
-	"github.com/Oracl0s/Mini-prodject/storage"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
 import "fmt"
 
 func main() {
-	// Создаем роутер
-	r := mux.NewRouter()
+	fmt.Println("hello world")
 
-	// Инициализируем наше "хранилище" в памяти
-	storage := storage.NewMemoryStorage()
-	// Инициализируем обработчики, передав им хранилище
-	taskHandler := handlers.NewTaskHandler(storage)
-
-	// Регистрируем HTTP маршруты
-	r.HandleFunc("/tasks", taskHandler.GetTasks).Methods("GET")
-	r.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
-	// r.HandleFunc("/tasks/{id}", taskHandler.GetTask).Methods("GET")
-	// r.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
-	// r.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
-
-	// Запускаем сервер на порту 8080
-	log.Println("Server starting on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
-	fmt.Println("Hello World")
 }
